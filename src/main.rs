09b5c4bb05521e1bc978b32f6a6315d184f16165
rachel-rose <ihateface@autistici.org>
2021-05-03 15:57:10 +0200

    simple encrypt-decrypt demo using sqlcipher

--- src/main.rs
+++ src/main.rs
@@ -5,39 +5,38 @@
 use rusqlite::{Connection, Result};
 
 fn main() -> Result<()> {
-    //let conn = Connection::open_in_memory()?;
     println!("Attempting to establish a connection...");
     let connector = connect()?;
     println!("Connection established");
     println!("Attempting to create an encrypted database...");
-    create_key(&connector);
-    test_key(&connector)?;
+    //create_key(&connector);
+    encrypt(&connector)?;
+    println!("Created encrypted database.");
+    println!("Attempting to decrypt database...");
+    decrypt(&connector)?;
+    println!("Decrypted database.");
     Ok(())
 }
 
 fn connect() -> Result<Connection> {
-    let path = "../src/db_test.db3";
+    let path = "/home/x/src/dbtest/src/db.db";
     let connector = Connection::open(&path);
     println!("Path created at {}", path);
     connector
 }
 
-fn create_key(conn: &Connection) {
-    match conn.execute("PRAGMA key = 'hellocanihaveasandwich'", []) {
-        Ok(key) => println!("Encrypted database n.{} was created", key),
-        Err(err) => println!("Failed to key database {}", err),
-    }
+fn encrypt(conn: &Connection) -> Result<()> {
+    conn.execute_batch(
+        "ATTACH DATABASE 'encrypted.db' AS encrypted KEY 'testkey';
+                SELECT sqlcipher_export('encrypted');
+                DETACH DATABASE encrypted;",
+    )
 }
 
-fn test_key(conn: &Connection) -> Result<()> {
-    match conn.execute(
-        "PRAGMA key = 'hellocanihaveafffsandwich';
-                       SELECT count(*) FROM sqlite_master",
-        [],
-    ) {
-        Ok(count) => println!("Open encrypted database n.{}. Key successful.", count),
-        Err(err) => println!("Test failed {}", err),
-    }
-
-    Ok(())
+fn decrypt(conn: &Connection) -> Result<()> {
+    conn.execute_batch(
+        "ATTACH DATABASE 'plaintext.db' AS plaintext KEY 'testkey';
+                SELECT sqlcipher_export('plaintext');
+                DETACH DATABASE plaintext;",
+    )
 }
